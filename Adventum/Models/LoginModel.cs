b75eb308ee9b6f67bdb8479774908e5641489faf
raevaaM <maria.raeva04@gmail.com>
2023-05-29 00:49:34 +0300

    add display name

--- Adventum/Models/LoginModel.cs
+++ Adventum/Models/LoginModel.cs
@@ -4,11 +4,13 @@ namespace Adventum.Models;
 
 public class LoginModel
 {
+    [Display(Name = "Потребителско име")]
     [Required] public string Username { get; set; }
 
     [Required]
     [DataType(DataType.Password)]
+    [Display(Name = "Парола")]
     public string Password { get; set; }
 
-    [Display(Name = "Remember me?")] public bool RememberMe { get; set; }
+    [Display(Name = "Запомни ме?")] public bool RememberMe { get; set; }
 }
--- Adventum/Models/RegisterModel.cs
+++ Adventum/Models/RegisterModel.cs
@@ -5,18 +5,18 @@ namespace Adventum.Models;
 public class RegisterModel
 {
     [Required]
-    [Display(Name = "Username")]
+    [Display(Name = "Потребителско име")]
     [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
     public string Username { get; set; }
 
     [Required]
     [MaxLength(25)]
-    [Display(Name = "First name")]
+    [Display(Name = "Име")]
     public string FirstName { get; set; }
 
     [Required]
     [MaxLength(25)]
-    [Display(Name = "Last name")]
+    [Display(Name = "Фамилия")]
     public string LastName { get; set; }
 
 
@@ -28,11 +28,11 @@ public class RegisterModel
     [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
         MinimumLength = 6)]
     [DataType(DataType.Password)]
-    [Display(Name = "Password")]
+    [Display(Name = "Парола")]
     public string Password { get; set; }
 
     [DataType(DataType.Password)]
-    [Display(Name = "Confirm password")]
+    [Display(Name = "Потвърди парола")]
     [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
     public string ConfirmPassword { get; set; }
 }
